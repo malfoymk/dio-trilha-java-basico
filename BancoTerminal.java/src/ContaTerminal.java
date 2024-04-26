1a9d39b7ae8e23fe4e05c5d1d2c6e8c2cbf413c3
Patrick Lopes <lopes.patrick4@gmail.com>
2024-04-25 22:24:19 -0300

    Add Desafio

---
+++ BancoTerminal.java/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o número da conta: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine(); // Consumir a nova linha pendente
+
+        System.out.print("Digite o número da agência: ");
+        String agency = scanner.nextLine();
+
+        System.out.print("Digite o nome do titular: ");
+        String accountHolder = scanner.nextLine();
+
+        System.out.print("Digite o saldo da conta: ");
+        double balance = Double.parseDouble(scanner.nextLine());
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco. Sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+                accountHolder, agency, accountNumber, balance);
+
+        scanner.close();
+    }
+}
